eb9ff7cefa190bba7009f120b1dcb159d029d6c1
PaulRoss1 <paulross992@gmail.com>
2022-03-30 02:17:44 +0200

    initial commit

---
+++ stript.js
@@ -0,0 +1,114 @@
+// Graph Settings
+
+const labels = [
+  "8:00",
+  "",
+  "9:00",
+  "",
+  "10:00",
+  "",
+  "11:00",
+  "",
+  "12:00",
+  "",
+  "13:00",
+  "",
+  "14:00",
+  "",
+  "15:00",
+  "",
+  "16:00",
+  "",
+  "17:00",
+  "",
+  "18:00",
+  "",
+  "19:00",
+  "",
+  "20:00",
+  "",
+  "21:00",
+  "",
+  "22:00",
+];
+const data = [
+  2, 5, 4, 3, 1, 2, 3, 10, 9, 8, 5, 4, 3, 1, 2, 4, 3, 8, 6, 7, 8, 8, 4, 4, 3, 7,
+  4, 3, 2,
+];
+
+const backgroundColors = [];
+
+for (i = 0; i < data.length; i++) {
+  if (data[i] < 4) {
+    backgroundColors.push("#e5b18b");
+  }
+  if (data[i] <= 7 && data[i] >= 4) {
+    backgroundColors.push("#df7e61");
+  }
+  if (data[i] > 7) {
+    backgroundColors.push("#cf461e");
+  }
+}
+
+console.log(backgroundColors);
+
+new Chart(document.getElementById("myChart"), {
+  type: "bar",
+  data: {
+    labels: labels,
+    datasets: [
+      {
+        label: ["Visitors"],
+        data: data,
+        backgroundColor: backgroundColors,
+        borderRadius: 20,
+        barPercentage: 1.0,
+        categoryPercentage: 1.0,
+      },
+    ],
+  },
+  options: {
+    maintainAspectRatio: false,
+    legend: {
+      display: true,
+      position: "bottom",
+    },
+    plugins: { legend: { display: false } },
+
+    scales: {
+      x: {
+        grid: {
+          display: false,
+        },
+        ticks: {
+          autoSkip: false,
+          maxRotation: 90,
+          minRotation: 90,
+        },
+      },
+      y: {
+        display: false,
+        grid: {
+          display: false,
+        },
+        ticks: {
+          display: false,
+        },
+      },
+    },
+  },
+});
+
+// Mail Form
+
+let mail = document.getElementById("mail");
+
+mail.addEventListener("click", function (event) {
+  document.getElementById("mail-form").style.display = "block";
+});
+
+let closeMail = document.getElementById("close-mail");
+
+closeMail.addEventListener("click", function (event) {
+  document.getElementById("mail-form").style.display = "none";
+});
